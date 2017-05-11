7f80800bc6f12a88f3c31f841f7da4f0cccf70da
Anna Karutina <anna.karutina@khk.ee>
2017-05-11 14:30:37 +0300

    Created form template and form objects

--- src/Application.php
+++ src/Application.php
@@ -1,9 +1,21 @@
 <?php
+
 namespace BookingApp;
 
 use Silex\Application as SilexApplication;
 use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\LocaleServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 /**
  * Custom Application class that hold our application specifix functionality.
@@ -19,13 +31,11 @@ class Application extends SilexApplication
         $this->configureControllers();
     }
 
-	/**
-	* Config app options and register services.
-	*/
-	private function configureServices()
-	{
-
-
+    /**
+     * Config app options and register services.
+     */
+    private function configureServices()
+    {
         $this['debug'] = true;
 
         $this->register(new TwigServiceProvider(), [
@@ -39,16 +49,19 @@ class Application extends SilexApplication
                 'path' => __DIR__.'/../database/app.db',
             ],
         ]);
+
+        $this->register(new FormServiceProvider());
+        $this->register(new LocaleServiceProvider());
+        $this->register(new TranslationServiceProvider(), [
+            'translator.domains' => [],
+        ]);
     }
 
-	/**
-	* Creates all needed tables to database if they don't exist.
-	*/
-	private function createDBTables()
-	{
-
-
-        // Creating a table if it doesn't exist yet
+    /**
+     * Creates all needed tables to database if they don't exist.
+     */
+    private function createDBTables()
+    {
         if (!$this['db']->getSchemaManager()->tablesExist('bookings')) {
             $this['db']->executeQuery("CREATE TABLE bookings (
                 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -67,15 +80,51 @@ class Application extends SilexApplication
         }
     }
 
-	/**
-	* Define all used routes and connect a route to its controller.
-	*/
-	private function configureControllers()
-	{
-
-
+    /**
+     * Define all used routes and connect a route to its controller.
+     */
+    private function configureControllers()
+    {
         $this->get('/bookings/create', function () {
-            return $this['twig']->render('base.html.twig');
+            $form = $this['form.factory']->createBuilder(FormType::class)
+                ->add('firstName', TextType::class, ['required' => true])
+                ->add('lastName', TextType::class, ['required' => true])
+                ->add('phone', TextType::class, ['required' => true])
+                ->add('email', TextType::class, ['required' => false])
+                ->add('birthday', DateType::class, [
+                    'required' => true,
+                    'widget' => 'single_text',
+                    'format' => 'dd.MM.yyyy',
+                ])
+                ->add('startDate', DateType::class, [
+                    'required' => true,
+                    'widget' => 'single_text',
+                    'format' => 'dd.MM.yyyy',
+                ])
+                ->add('endDate', DateType::class, [
+                    'required' => true,
+                    'widget' => 'single_text',
+                    'format' => 'dd.MM.yyyy',
+                ])
+                ->add('arrivalTime', TimeType::class, ['required' => true])
+                ->add('nrOfPeople', IntegerType::class, ['required' => true])
+                ->add('payingMethod', ChoiceType::class, [
+                    'choices' => [
+                        'cash' => 'cash',
+                        'transfer' => 'transfer',
+                    ],
+                    'required' => true,
+                ])
+                ->add('additionalInformation', TextareaType::class, [
+                    'required' => false
+                ])
+                ->add('submit', SubmitType::class, ['label' => 'Send booking'])
+                ->getForm()
+            ;
+
+            return $this['twig']->render('form.html.twig', [
+                'form' => $form->createView()
+            ]);;
         });
     }
 }
