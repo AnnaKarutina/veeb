3d9f7fb0b541678903124dfaaf5695dff41b5a89
Anna Karutina <anna.karutina@khk.ee>
2017-05-11 11:40:36 +0300

    Install twig and create base.html.twig

--- web/index.php
+++ web/index.php
@@ -1,16 +1,29 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
+
 // loome Silex rakenduse objekti
 $app = new Silex\Application();
+
 // lubame silumist 
 $app['debug'] = true;
+
+// anname teada, et html failid hakkavad
+// asuma views kataloogis
+$app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__.'/../views', ]);
+
 // kasutame get meetodi, mille parameetrid on
 // hello - kontroller, mis tuleb tööle
 // funktsioon, mis tuleb tööle, kui 
 // aadressireal on kirjutatud hello
-$app->get('/hello', function(){
-	return "Hello World!";
+
+// nüüd hello asemel on views kataloogist
+// mallide lugemine
+// siin on võetud kasutusele alnonüümne funktsioon
+// $app võimaluste ja omaduste pärimiseks
+$app->get('/bookings/create', function() use ($app){
+	return $app['twig']->render('base.html.twig');
 });
+
 // käivitame Silexi rakendus
 $app->run();
 ?>
