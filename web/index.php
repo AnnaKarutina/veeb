89b9778dd9c22eeb039ab940b0092a0f08ae139a
Anna Karutina <anna.karutina@khk.ee>
2017-05-11 12:46:31 +0300

    Created database connection and bookings table

--- web/index.php
+++ web/index.php
@@ -10,6 +10,33 @@ $app['debug'] = true;
 // anname teada, et html failid hakkavad
 // asuma views kataloogis
 $app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__.'/../views', ]);
+// lubame andmebaasi teeki kasutamist
+// ning paneme antud andmebaasi konfiguratsiooni
+// kirja
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+		'db.options' => [
+			'driver' => 'pdo_sqlite',
+			'path' => __DIR__.'/../database/app.db',
+		],
+	]);
+// lisame andmebaasi tabeli
+if (!$app['db']->getSchemaManager()->tablesExist('bookings')) {
+    $app['db']->executeQuery("CREATE TABLE bookings (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstName VARCHAR(40) NOT NULL,
+        lastName VARCHAR(40) NOT NULL,
+        phone VARCHAR(10) NOT NULL,
+        email VARCHAR(20) DEFAULT NULL,
+        birthday DATE NOT NULL,
+        startDate DATE NOT NULL,
+        endDate DATE NOT NULL,
+        arrivalTime TIME DEFAULT NULL,
+        additionalInformation TEXT,
+        nrOfPeople INT NOT NULL,
+        payingMethod VARCHAR(10) NOT NULL
+    );");
+}
+
 
 // kasutame get meetodi, mille parameetrid on
 // hello - kontroller, mis tuleb tööle
